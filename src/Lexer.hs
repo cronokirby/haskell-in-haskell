28b56ca142137bf71e4dede6e0a88d67f9b1968a
Lúcás Meier <cronokirby@gmail.com>
2020-10-16 21:02:34 +0200

    WIP: Layouts

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -92,98 +92,127 @@ data Token
   | Equal -- `=`
   | Dot -- `.`
   | Dollar -- `$`
+  | StartOfFile
   | IntLitt Int -- An integer litteral
   | TypeName String -- A reference to some kind of type name
   | Name String -- A reference to some kind of name
   deriving (Eq, Show)
 
 -- Lex out one of the tokens in our language
-token :: Lexer Token
+token :: Lexer (Token, String)
 token = keywords <|> operators <|> intLitt <|> typeName <|> name
   where
-    keywords :: Lexer Token
+    with :: Functor f => b -> f a -> f (b, a)
+    with b = fmap (\a -> (b, a))
+    keywords :: Lexer (Token, String)
     keywords =
       oneOf
-        [ Let <$ string "let",
-          Where <$ string "where",
-          In <$ string "in",
-          Data <$ string "data",
-          Type <$ string "type",
-          If <$ string "if",
-          Then <$ string "then",
-          Else <$ string "else",
-          Case <$ string "case",
-          Of <$ string "of",
-          Underscore <$ string "_"
+        [ Let `with` string "let",
+          Where `with` string "where",
+          In `with` string "in",
+          Data `with` string "data",
+          Type `with` string "type",
+          If `with` string "if",
+          Then `with` string "then",
+          Else `with` string "else",
+          Case `with` string "case",
+          Of `with` string "of",
+          Underscore `with` string "_"
         ]
-    operators :: Lexer Token
+    operators :: Lexer (Token, String)
     operators =
       oneOf
-        [ OpenParens <$ char '(',
-          CloseParens <$ char ')',
-          OpenBrace <$ char '{',
-          CloseBrace <$ char '}',
-          Semicolon <$ char ';',
-          DoubleColon <$ string "::",
-          ThinArrow <$ string "->",
-          VBar <$ char '|',
-          BSlash <$ char '\\',
-          FSlash <$ char '/',
-          Plus <$ char '+',
-          Dash <$ char '-',
-          Asterisk <$ char '*',
-          Equal <$ char '=',
-          Dot <$ char '.',
-          Dollar <$ char '$'
+        [ OpenParens `with` string "(",
+          CloseParens `with` string ")",
+          OpenBrace `with` string "{",
+          CloseBrace `with` string "}",
+          Semicolon `with` string ";",
+          DoubleColon `with` string "::",
+          ThinArrow `with` string "->",
+          VBar `with` string "|",
+          BSlash `with` string "\\",
+          FSlash `with` string "/",
+          Plus `with` string "+",
+          Dash `with` string "-",
+          Asterisk `with` string "*",
+          Equal `with` string "=",
+          Dot `with` string ".",
+          Dollar `with` string "$"
         ]
-    intLitt :: Lexer Token
-    intLitt = some (satisfies isDigit) |> fmap (read >>> IntLitt)
+    intLitt :: Lexer (Token, String)
+    intLitt = some (satisfies isDigit) |> fmap (\x -> (IntLitt (read x), x))
     continuesName :: Lexer Char
     continuesName = satisfies isAlphaNum <|> char '\''
-    typeName :: Lexer Token
-    typeName = (liftA2 (:) (satisfies isUpper) (many continuesName)) |> fmap TypeName
-    name :: Lexer Token
-    name = (liftA2 (:) (satisfies isLower) (many continuesName)) |> fmap Name
+    typeName :: Lexer (Token, String)
+    typeName = (liftA2 (:) (satisfies isUpper) (many continuesName)) |> fmap (\x -> (TypeName x, x))
+    name :: Lexer (Token, String)
+    name = (liftA2 (:) (satisfies isLower) (many continuesName)) |> fmap (\x -> (Name x, x))
 
 -- A raw token is either a "real" token, or some whitespace that we actually want to ignore
-data RawToken = Blankspace String | Newline | RawToken Token
+data RawToken = Blankspace String | Newline | RawToken Token String
 
 rawLexer :: Lexer [RawToken]
-rawLexer = some (fmap RawToken token <|> whitespace)
+rawLexer = fmap (RawToken StartOfFile "" :) (some item)
   where
+    item = whitespace <|> fmap (uncurry RawToken) token
     whitespace = blankspace <|> newline
     blankspace = Blankspace <$> some (satisfies (\x -> isSpace x && x /= '\n'))
     newline = Newline <$ char '\n'
 
--- Lex a specific string, producing a list of tokens if no errors occurred.
-lexer :: String -> Maybe [Token]
-lexer = runLexer rawLexer >>> fmap (fst >>> filterTokens)
-  where
-    filterTokens =
-      let go (RawToken x) acc = x : acc
-          go (Blankspace _) acc = acc
-          go Newline acc = acc
-       in foldr go []
-
--- Represents a position a token can have.
---
--- A token is either at the start of a line, in which case we care about its column,
--- or in the middle of a line, in which case we don't care
-data Position = Start Int | Middle
+data LinePosition = Start | Middle
 
 -- Some type annotated with a position
-data Positioned a = Positioned a Position
+data Positioned a = Positioned a LinePosition Int
 
 -- Take tokens and whitespace, and return positioned tokens, with whitespace filtered out
 position :: [RawToken] -> [Positioned Token]
-position = foldr go (Start 0, []) >>> snd
+position = foldr go ((Start, 0), []) >>> snd
   where
-    eat :: Position -> RawToken -> (Position, Maybe (Positioned Token))
-    eat (Start c) (Blankspace s) = (Start (c + length s), Nothing)
-    eat Middle (Blankspace _) = (Middle, Nothing)
-    eat _ Newline = (Start 0, Nothing)
-    eat p (RawToken t) = (Middle, Just (Positioned t p))
-    go :: RawToken -> (Position, [Positioned Token]) -> (Position, [Positioned Token])
+    eat :: (LinePosition, Int) -> RawToken -> ((LinePosition, Int), Maybe (Positioned Token))
+    eat _ Newline = ((Start, 0), Nothing)
+    eat (pos, col) (Blankspace s) = ((pos, col + length s), Nothing)
+    eat (pos, col) (RawToken t s) = ((Middle, col + length s), Just (Positioned t pos col))
+    go :: RawToken -> ((LinePosition, Int), [Positioned Token]) -> ((LinePosition, Int), [Positioned Token])
     go raw (p, acc) = case eat p raw of
       (p', Just tok) -> (p', tok : acc)
       (p', Nothing) -> (p', acc)
+
+-- A layout is either one explicitly declared by the user, or implicitly declared at a certain column
+data Layout = Explicit | Implicit Int
+
+layout :: [Positioned Token] -> Maybe [Token]
+layout tokens = go tokens []
+  where
+    startsLayout :: Token -> Bool
+    startsLayout t = elem t [Let, Where, Of, StartOfFile]
+    go :: [Positioned Token] -> [Layout] -> Maybe [Token]
+    -- An explicit } must close a corresponding explicit layout
+    go (Positioned CloseBrace _ _ : ts) (Explicit : ls) = fmap (CloseBrace :) (go ts ls)
+    go (Positioned CloseBrace _ _ : _) _ = Nothing
+    -- An explicit { starts an explicit layout
+    go (Positioned OpenBrace _ _ : ts) ls = fmap (OpenBrace :) (go ts (Explicit : ls))
+    -- If we see a token that starts a layout, three things can happen
+    go (Positioned starter _ _ : tok@(Positioned t _ n) : ts) ls' | startsLayout starter = case ls' of
+      -- If no layouts exist yet, then we can start at any indentation
+      [] -> fmap ([starter, OpenBrace, t] ++) (go ts [Implicit n])
+      -- Otherwise we need to be further indented to start a new layout
+      Implicit m : ls | n > m -> fmap ([starter, OpenBrace, t] ++) (go ts (Implicit n : Implicit m : ls))
+      -- If we're less indented, that means that we've skipped a layout
+      ls -> fmap ([starter, OpenBrace, CloseBrace] ++) (go (tok : ts) ls)
+    -- If a starting token is at the same level of implicit indentation, that continues the layout
+    go (Positioned t Start n : ts) (Implicit m : ls) | n == m = fmap ([Semicolon, t] ++) (go ts (Implicit m : ls))
+    -- If a starting token has less than the implicit indentation, then close that layout
+    go (tok@(Positioned _ Start n) : ts) (Implicit m : ls) | n < m = fmap (CloseBrace :) (go (tok : ts) ls)
+    -- If nothing else applies, we just want to emit the tokens we see
+    go (Positioned t _ _ : ts) ls = fmap (t :) (go ts ls)
+    -- Close all of the implicit layouts
+    go [] (Implicit _ : ls) = fmap (OpenBrace :) (go [] ls)
+    -- Any remaining explicit layouts are unclosed, and an error
+    go [] (Explicit : _) = Nothing
+    go [] [] = Just []
+
+-- Lex a specific string, producing a list of tokens if no errors occurred.
+lexer :: String -> Maybe [Token]
+lexer input = do
+  (raw, _) <- runLexer rawLexer input
+  raw |> position |> layout
