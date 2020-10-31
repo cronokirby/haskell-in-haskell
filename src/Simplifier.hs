4dc296ad3799a65d5bcf37c01ed927c2830c2de0
Lúcás Meier <cronokirby@gmail.com>
2020-10-31 18:32:43 +0100

    Calculate tree depth correctly

--- src/Simplifier.hs
+++ src/Simplifier.hs
@@ -146,7 +146,7 @@ data PatternTree = Leaf Expr | Branch [(Pattern, PatternTree)] | Empty
 -- This is useful to know the number of lambda arguments we might need
 treeDepth :: PatternTree -> Int
 treeDepth (Leaf _) = 0
-treeDepth (Branch bs) = map (snd >>> treeDepth) bs |> maximum
+treeDepth (Branch bs) = map (snd >>> treeDepth) bs |> maximum |> (+1)
 
 -- True if a given pattern is equivalent, or captures more values than another
 covers :: Pattern -> Pattern -> Bool
