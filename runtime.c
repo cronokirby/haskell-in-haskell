103b4888205649ffff6fdf9b6e83bcd9a657aaef
Lúcás Meier <cronokirby@gmail.com>
2020-11-27 13:36:02 +0100

    WIP: Add some basic runtime boilerplate

---
+++ runtime.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void panic(const char *message) {
+  puts("PANIC:");
+  puts(message);
+  exit(-1);
+}
+
+const size_t BASE_HEAP_SIZE = 1 << 16;
+
+// The heap is a pointer to pointers, basically
+void **H;
+void **H_base;
+size_t H_size;
+
+void H_alloc(size_t count) {
+  size_t diff = H - H_base;
+  if (diff < count) {
+    H_base = realloc(H_base, H_size);
+    H = H_base + H_size + diff;
+    H_size *= 2;
+  }
+}
+
+const size_t BASE_STACK_SIZE = 1 << 10;
+
+void **SA;
+void **SA_base;
+size_t SA_size;
+
+void SA_alloc(size_t count) {
+  size_t diff = SA - SA_base;
+  if (diff < count) {
+    SA_base = realloc(SA_base, SA_size);
+    SA = SA_base + SA_size + diff;
+    SA_size *= 2;
+  }
+}
+
+void **SB;
+void **SB_base;
+size_t SB_size;
+
+void SB_alloc(size_t count) {
+  size_t diff = SB - SB_base;
+  if (diff < count) {
+    SB_base = realloc(SB_base, SB_size);
+    SB = SB_base + SB_size + diff;
+    SB_size *= 2;
+  }
+}
+
+void setup() {
+  H_base = malloc(sizeof(void*) * BASE_HEAP_SIZE);
+  if (H_base == NULL) {
+    panic("Failed to initialize heap");
+  }
+  H = H_base + BASE_HEAP_SIZE;
+  H_size = BASE_HEAP_SIZE;
+
+  SA_base = malloc(sizeof(void*) * BASE_STACK_SIZE);
+  if (SA_base == NULL) {
+    panic("Failed to initialize stack A");
+  }
+  SA = SA_base + BASE_STACK_SIZE;
+  SA_size = BASE_STACK_SIZE;
+
+  SB_base = malloc(sizeof(void*) * BASE_STACK_SIZE);
+  if (SB_base == NULL) {
+    panic("Failed to initialize stack A");
+  }
+  SB = SB_base + BASE_STACK_SIZE;
+  SB_size = BASE_STACK_SIZE;
+}
